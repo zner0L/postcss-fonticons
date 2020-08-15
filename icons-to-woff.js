c6becc0bb2895968635ebdcfe9bf11c4e91db3a0
Lorenz Sieben <me@lorenz-sieben.com>
2020-08-15 20:46:40 +0200

    Updated to the recent version of iconfont-webpack-plugin

--- icons-to-woff.js
+++ icons-to-woff.js
@@ -2,7 +2,6 @@
 
 'use strict';
 const assert = require('assert');
-const fs = require('fs');
 const path = require('path');
 const Svgicons2svgfont = require('svgicons2svgfont');
 const svg2ttf = require('svg2ttf');
@@ -10,11 +9,12 @@ const ttf2woff = require('ttf2woff');
 const Readable = require('stream').Readable;
 
 /**
- * @param  {String[]} icons          Array of icon file paths
- * @param  {Object} options          SVG-Font options
- * @return {Promise<String>}         Base64 encoded font
+ * @param  {typeof import("fs")} fs  An input file system
+ * @param  {String[]} icons Array of icon file paths
+ * @param  {{name: string, enforcedSvgHeight?: number}} options SVG-Font options
+ * @return {Promise<String>} Base64 encoded font
  */
-module.exports = function createIconFont (icons, options) {
+module.exports = function createIconFont(fs, icons, options) {
     assert(typeof options === 'object', 'Options are mandatory.');
     icons = icons.map((iconPath) => path.resolve('.', iconPath));
     return new Promise((resolve, reject) => {
@@ -22,8 +22,8 @@ module.exports = function createIconFont (icons, options) {
             name: options.name,
             normalize: true,
             fontHeight: options.enforcedSvgHeight ? options.enforcedSvgHeight : undefined,
-            log: function () {},
-            error: function (err) {
+            log: function () { },
+            error: /** @param {any} err */function (err) {
                 reject(err);
             }
         });
@@ -39,25 +39,21 @@ module.exports = function createIconFont (icons, options) {
                 reject(err);
             });
         icons.forEach((filename, i) => {
-            const glyph = new Readable();
-            glyph._read = function noop () {};
-            glyph.metadata = {
-                unicode: [String.fromCodePoint('\ue000'.charCodeAt(0) + i)],
-                name: 'i' + i
-            };
+            const glyph = Object.assign(new Readable(), {
+                _read: function noop() { },
+                metadata: {
+                    unicode: [String.fromCodePoint('\ue000'.charCodeAt(0) + i)],
+                    name: 'i' + i
+                }
+            });
             fontStream.write(glyph);
-            fs.readFile(filename, function (err, result) {
+            fs.readFile(filename, function (err, svgBuffer) {
                 if (err) {
                     return reject(err);
                 }
-                result = result.toString();
-                const iconHeight = getSvgHeight(result, filename);
-                if (!iconHeight) {
-                    return reject(new Error(`SVG font generation failed as icon "${filename}" does not have a height.`));
-                }
                 // prevent svgs with fill="none" from beeing translated into an empty symbol
-                result = result.replace(/\sfill\s*=\s*["']?none['"]?/ig, '');
-                glyph.push(result);
+                const svgCode = svgBuffer.toString().replace(/\sfill\s*=\s*["']?none['"]?/ig, '');
+                glyph.push(svgCode);
                 glyph.push(null);
             });
         });
@@ -67,18 +63,3 @@ module.exports = function createIconFont (icons, options) {
         .then((ttfFont) => ttf2woff(ttfFont).buffer)
         .then((woffFont) => Buffer.from(woffFont).toString('base64'));
 };
-
-/**
- * Reads the height of the svg
- *
- * @param  {String} svg      the svg content
- * @param  {String} filename the file name for error reporting
- * @return {Number}          height
- */
-function getSvgHeight (svg, filename) {
-    const parseSvg = /<svg[^>]+height\s*=\s*["']?(\d+)\s*(pt|px|)["']?/i.exec(svg);
-    if (!parseSvg) {
-        throw new Error(`could not read height for '${filename}'.`);
-    }
-    return parseSvg[1];
-}
